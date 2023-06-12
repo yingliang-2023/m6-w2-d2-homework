35dbd3112d0cd045fec2412f243c75a8dc937c53
YING LIANG <y.liang.168@westcliff.edu>
2023-06-11 23:55:49 -0500

    css

--- redux-counter/src/Counter.js
+++ redux-counter/src/Counter.js
@@ -1,28 +1,41 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import './App.css';
+
 
 class Counter extends React.Component {
-    state = {count:0};
+    // state = {count:0};
+    
 
     increment = ()=>{
-        this.setState({
-            count:this.state.count+1
-        })
+        // this.setState({
+        //     count:this.state.count+1
+        // })
+        this.props.dispatch({type:'INCREMENT'})
     };
 
     decrement = ()=>{
-        this.setState({
-            count:this.state.count-1
-        })
+        // this.setState({
+        //     count:this.state.count1
+        // })
+        this.props.dispatch({type:'DECREMENT'})
     };
+
+    reset = ()=>{
+        // this.setState({
+        //     count:this.state.count1
+        // })
+        this.props.dispatch({type:'RESET'})
+    };
+
 
     render() {
         return (
             <div>
                 <h2>Counter</h2>
-
-                <div>
-            
-                     <span>{this.state.count}</span>
+ 
+                <div className='counterDiv'>
+                     <span>{this.props.count}</span>
                      <button onClick={this.decrement}>-</button>
                      <button onClick={this.increment}>+</button>
                 </div>
@@ -31,4 +44,12 @@ class Counter extends React.Component {
     }
 }
 
-export default Counter;
+//Add the mapStateToProps function:
+function mapStateToProps(state) {
+    return {
+        count:state.count
+    }
+};
+
+
+export default connect(mapStateToProps)(Counter);
--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Counter from './Counter';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+// import {legacy_createStore as createStore} from 'redux'
 import './index.css';
 
+//declare initial state
+const initialState = {
+  count:0
+};
+
+/*declare action: action is a JS object that describes a change we want to make
+and must have a 'type' property*/
+
+//take previous state as argument and return new state
+
+function reducer(state=initialState,action){
+  console.log(action);
+   switch (action.type){
+      case 'INCREMENT':
+        return {
+        count:state.count+1
+        };
+      case 'DECREMENT':
+        return{
+          count:state.count-1
+        };
+      case 'RESET':
+        return state;
+      default:
+        return state;
+    };
+};
+
+//changes happen one way only: action->reducer->new state
+
+const store = createStore(reducer);
+
 const App= ()=>(
-  <div>
+  <Provider store={store}>
     <Counter/>
-  </div>
+  </Provider>
 );
 
-render(<App />, document.getElementById('root'));
+render(<App />,document.getElementById('root'));
 
