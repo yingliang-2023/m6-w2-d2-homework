a56ebff10615a94163a853d42b900786e9667f0c
YING LIANG <y.liang.168@westcliff.edu>
2023-06-11 22:44:31 -0500

    commit2

---
+++ redux-counter/src/Counter.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class Counter extends React.Component {
+    state = {count:0};
+
+    increment = ()=>{
+        this.setState({
+            count:this.state.count+1
+        })
+    };
+
+    decrement = ()=>{
+        this.setState({
+            count:this.state.count-1
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>Counter</h2>
+
+                <div>
+            
+                     <span>{this.state.count}</span>
+                     <button onClick={this.decrement}>-</button>
+                     <button onClick={this.increment}>+</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Counter;
